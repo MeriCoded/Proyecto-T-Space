6355d0feda8bcb5d11f66fd48c3cdfb24fb2e84b
MeriCoded <diosameri1@gmail.com>
2025-05-26 11:32:37 -0300

    meteoritos
    meteoritos

--- Proyecto T Space/Touhou pero con naves to cool osi nombre largo para que quede menos profesional.py
+++ Proyecto T Space/Touhou pero con naves to cool osi nombre largo para que quede menos profesional.py
@@ -7,7 +7,7 @@ import math
 
 from settings import Settings
 from estelar import Estelar
-from alien import Alien
+from meteor import Meteor
 from space import Space
 
 class Window:
@@ -20,12 +20,14 @@ class Window:
         self.screen_rect = self.screen.get_rect()
         self.estelar = Estelar(self)
         self.bullets = pygame.sprite.Group()
-        self.aliens = pygame.sprite.Group()
+        self.meteor = pygame.sprite.Group()
         self.space = Space(self.screen, self.settings)
+        self.meteors_pequenos = pygame.sprite.Group()
+        self.meteors_grandes = pygame.sprite.Group()
 
         for i in range(4):
-            alien = Alien(self, 40 + i*70, 100)
-            self.aliens.add(alien)
+            meteor = Meteor(self, 40 + i*70, 100, tipo="M4")
+            self.meteor.add(meteor)
 
     def run(self):
         running = True
@@ -38,16 +40,21 @@ class Window:
 
             self.estelar.update_estelar()
             self.bullets.update()
-            self.aliens.update()
+            self.meteor.update()
+            self.meteors_pequenos.update()
             self.space.draw()
 
-            pygame.sprite.groupcollide(self.aliens, self.bullets, True, True)
+            for meteor in pygame.sprite.groupcollide(self.meteor, self.bullets, True, True):
+                meteor.break_apart()
+            
+            pygame.sprite.groupcollide(self.meteors_pequenos, self.bullets, True, True)
 
             self.estelar.draw_estelar()
             self.bullets.draw(self.screen)
             for bullet in self.bullets:
                 bullet.draw_bullet()
-            self.aliens.draw(self.screen)
+            self.meteor.draw(self.screen)
+            self.meteors_pequenos.draw(self.screen)
 
             pygame.display.flip()
             self.clock.tick(self.settings.fps)
--- Proyecto T Space/space.py
+++ Proyecto T Space/space.py
@@ -7,16 +7,19 @@ class Space:
     def __init__(self, screen, settings):
         self.screen = screen
         self.settings = settings
-        self.bg = pygame.image.load("assets/Espacio/bg1.png").convert()
-        self.bg_height = self.bg.get_height()
-        self.scroll_speed = 0
+        self.bg_images = []
+        for i in range(1, 4): #las imagenes del fondo que voy a usar (no voy a usar otra)
+            bg_image = pygame.image.load(f"assets/Espacio/bg{i}.png").convert_alpha()
+            self.bg_images.append(bg_image)
+        self.bg_height = self.bg_images[0].get_height()
+        
+        self.speeds = [4, 3, 1] 
+        self.offsets = [0, 0, 0] #posicion inicial de las nebulosas y los planetas (obviamente tambien del espacio)
 
-    def draw(self):
+    def draw(self): #calculos complicados para que el fondo scrolle de manera infinita (y de resultados redondos)
         bg_loop = math.ceil(self.settings.screen_height / self.bg_height) + 1
-        for i in range(bg_loop):
-            y = (i * self.bg_height + self.scroll_speed) % (self.bg_height * bg_loop) - self.bg_height
-            self.screen.blit(self.bg, (0, y))
-
-        self.scroll_speed += 5
-        if self.scroll_speed >= self.bg_height:
-            self.scroll_speed = 0
+        for idx, bg in enumerate(self.bg_images):
+            for i in range(bg_loop):
+                y = (i * self.bg_height + self.offsets[idx]) % (self.bg_height * bg_loop) - self.bg_height
+                self.screen.blit(bg, (0, y))
+            self.offsets[idx] = (self.offsets[idx] + self.speeds[idx]) % self.bg_height
