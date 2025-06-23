07eeb0de2c45b2b2df542dde481a087d3a5c8ceb
Alaska <julietaespositox@hotmail.com>
2025-06-23 20:20:30 -0300

    Add files via upload

---
+++ constants.py
@@ -0,0 +1,7 @@
+M1_TIME = 0 
+M2_TIME = 60 #reemplazar por 60
+M3_TIME = 120 #reemplazar por 120
+M4_TIME = 180 #reemplazar por 180
+
+LEVEL_2 = 560
+LEVEL_3 = 1440
---
+++ estelar.py
@@ -0,0 +1,124 @@
+import pygame
+import time
+from constants import *
+from settings import Settings
+
+class Estelar(pygame.sprite.Sprite):
+    #La nave se llamara Estelar
+    def __init__(self, game):
+        super().__init__()
+        self.game = game
+        self.image = pygame.image.load("Assets/Player/Estelar7.png").convert_alpha()
+        self.image = pygame.transform.scale(self.image, (80, 70))
+        self.rect = self.image.get_rect()
+        self.rect.centerx = game.settings.screen_width // 2
+        self.rect.bottom = game.settings.screen_height - 10
+        self.health = 500
+        self.lifes = 3
+        self.speed = 5
+        self.level = 0
+        self.direction = pygame.math.Vector2(0, 0)
+        self.shoot_cooldown = 0
+        self.burst_count = 0
+        self.burst_timer = 0
+        self.shoot_sound = pygame.mixer.Sound("Assets/Sonidos/disparo.wav")
+        self.shoot_sound.set_volume(0.1)
+        # Utilizamos mascaras para generar colisiones pixel-perfect
+        #self.mask = pygame.mask.from_surface(self.image) - No es necesario escribirlo
+
+    def draw_estelar(self):
+        self.game.screen.blit(self.image, self.rect)
+
+    def update_estelar(self):
+        self.input()
+        self.move()
+        self.handle_burst()
+
+        #Controles
+    def input(self):
+        keys = pygame.key.get_pressed()
+        self.direction.x = int(keys[pygame.K_RIGHT]) - int(keys[pygame.K_LEFT])
+        self.direction.y = int(keys[pygame.K_DOWN]) - int(keys[pygame.K_UP])
+        
+        if keys[pygame.K_SPACE]:
+            if self.shoot_cooldown <= 0 and self.burst_count == 0:
+                self.burst_count = 1  # de cuanto quiero la rafaga
+                self.burst_timer = 0
+
+    def handle_burst(self):
+        if self.burst_count > 0:
+            if self.burst_timer <= 0:
+                self.shoot()
+                self.burst_count -= 1
+                self.burst_timer = 5  # frames entre balas
+            else:
+                self.burst_timer -= 1
+        if self.shoot_cooldown > 0:
+            self.shoot_cooldown -= 1  
+
+    def move(self):
+        if self.direction.x and self.direction.y:
+            self.direction.normalize_ip()
+        self.rect.x += self.direction.x * self.game.settings.estelar_speed
+        self.rect.y += self.direction.y * self.game.settings.estelar_speed
+        if self.rect.left < 0:
+            self.rect.left = 0
+        elif self.rect.right > self.game.settings.screen_width:
+            self.rect.right = self.game.settings.screen_width
+        if self.rect.top < 0:
+            self.rect.top = 0
+        elif self.rect.bottom > self.game.settings.screen_height:
+            self.rect.bottom = self.game.settings.screen_height
+
+    def shoot(self):
+        score = self.game.score
+        if score >= LEVEL_3:
+            level = 3
+        elif score >= LEVEL_2:
+            level = 2
+        else:
+            level = 1
+        
+        if level == 1:
+            bullet = Bullet(self.game, self.rect.centerx, self.rect.top)
+            self.game.bullets.add(bullet)
+        
+        elif level == 2:
+            bullet1 = Bullet(self.game, self.rect.centerx - 10, self.rect.top)
+            bullet2 = Bullet(self.game, self.rect.centerx + 10, self.rect.top)
+            self.game.bullets.add(bullet1, bullet2)
+            
+        elif level == 3:
+            bullet1 = Bullet(self.game, self.rect.centerx, self.rect.top)
+            bullet2 = Bullet(self.game, self.rect.centerx - 15, self.rect.top + 5)
+            bullet3 = Bullet(self.game, self.rect.centerx + 15, self.rect.top + 5)
+            self.game.bullets.add(bullet1, bullet2, bullet3)
+        
+        self.shoot_sound.play()  # Reproduce el sonido aquí
+        if self.burst_count == 1:
+            self.shoot_cooldown = 15  # cooldown entre ráfagas para que no sea tremendo spam (temporal)
+
+        
+            
+import pygame.sprite
+            
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self, game, x, y):
+        super().__init__()
+        self.game = game
+        self.image = pygame.image.load("Assets/Player/Bullet.png").convert_alpha()
+        self.image = pygame.transform.scale(self.image, (10, 21))
+        self.rect = self.image.get_rect()
+        self.rect.centerx = x
+        self.rect.bottom = y
+        self.speed = game.settings.bullet_speed
+        self.damage = 10
+        #self.mask = pygame.mask.from_surface(self.image) - No es necesario escribirlo
+
+    def draw_bullet(self):
+        self.game.screen.blit(self.image, self.rect)
+
+    def update(self):
+        self.rect.y -= self.speed
+        if self.rect.bottom < 0:
+            self.kill()  # Elimina las balitas
